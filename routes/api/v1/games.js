eb8f00205d71787a62166b0259997c4827fc9d67
JennicaStiehl <jennica.stiehl@gmail.com>
2019-06-14 17:58:12 -0600

    adds read routes

---
+++ routes/api/v1/games.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+var Game = require('../../../models').Game;
+/* GET all games */
+router.get("/", function(req, res, next) {
+  Game.findAll()
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+/*POST new game*/
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+module.exports = router; //this should stay at the bottom of the file
